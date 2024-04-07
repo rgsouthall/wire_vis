fe7efc0895ad5a38e53d7deaeb0916d25cbad060
Ryan Southall <rgsouthall@gmail.com>
2024-04-07 14:13:41 +0100

    Add jitter

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ bl_info = {"name": "Wire Visualiser",
            "tracker_url": "",
            "category": "Object"}
 
-import bpy, gpu, bmesh, mathutils, datetime
+import bpy, gpu, bmesh, mathutils, datetime, random
 from math import pi, radians
 from bpy.props import BoolProperty, PointerProperty, FloatVectorProperty, FloatProperty, IntProperty, EnumProperty
 from bpy.types import Panel, PropertyGroup, Operator, SpaceView3D
@@ -56,6 +56,7 @@ def get_all_children(col):
     for child in col.children:
         yield from get_all_children(child)
 
+
 class WIREVIS_Scene_Settings(PropertyGroup):
     wv_display: BoolProperty(name='', default=False, description='Display wire')
     update: BoolProperty(name='', default=False, description='Update wire')
@@ -71,12 +72,15 @@ class WIREVIS_Scene_Settings(PropertyGroup):
     wv_le: BoolProperty(name='', default=False, description='Lone edges', update=wire_update)
     wv_len: FloatProperty(name="mm", description="Wire length cutoff", default=10, min=0.0, update=wire_update)
     wv_rotate: FloatProperty(name="", description="Twist angle", default=0, min=0, max=360, update=wire_update)
+    wv_jitter: FloatProperty(name='', description='Random jitter', default=0, min=0, precision=4, update=wire_update)
+    wv_jitter_type: EnumProperty(name='', items=[('0', 'Edge', 'Edge jitter'), ('1', 'Vertex', 'Vertex')], description='Type of jitter', default='0', update=wire_update)
+
 
 class WIREVIS_Object_Settings(PropertyGroup):
     wv_bool: BoolProperty(name='', default=False, description='Display wire', update=wire_update)
     wv_colour: FloatVectorProperty(size=4, name="", attr="Colour", default=[0.0, 0.0, 0.0, 1.0], subtype='COLOR', min=0, max=1, update=wc_update)
     wv_extend: FloatProperty(name="", description="Wire extension", default=0, min=0, max=1, update=wire_update)
-    wv_extend_type: EnumProperty(name='', items=[('0', 'Relative', 'Relative extend'), ('1', 'Absolute', 'Absolute extend')], description='Type pf wire extend', default='0', update=wire_update)
+    wv_extend_type: EnumProperty(name='', items=[('0', 'Relative', 'Relative extend'), ('1', 'Absolute', 'Absolute extend')], description='Type of wire extend', default='0', update=wire_update)
     wv_angle: FloatProperty(name="", description="Wire angle", default=45, min=0, max=180, update=wire_update)
     wv_material: BoolProperty(name='', default=False, description='Material boundary', update=wire_update)
     wv_dia: FloatProperty(name="mm", description="Wire diameter", default=20, min=0.1, max=1000, update=wire_update)
@@ -84,6 +88,9 @@ class WIREVIS_Object_Settings(PropertyGroup):
     wv_le: BoolProperty(name='', default=False, description='Lone edges', update=wire_update)
     wv_len: FloatProperty(name="mm", description="Wire length cutoff", default=10, min=0.0, update=wire_update)
     wv_rotate: FloatProperty(name="", description="Twist angle", default=0, min=0, max=360, update=wire_update)
+    wv_jitter: FloatProperty(name='', description='Random jitter', default=0, min=0, precision=4, update=wire_update)
+    wv_jitter_type: EnumProperty(name='', items=[('0', 'Edge', 'Edge jitter'), ('1', 'Vertex', 'Vertex')], description='Type of jitter', default='0', update=wire_update)
+
 
 class WIREVIS_PT_scene(Panel):
     '''WireVis 3D view panel'''
@@ -109,7 +116,12 @@ class WIREVIS_PT_scene(Panel):
             if swv.wv_extend:
                 newrow(layout,  'Extend type:',  swv,  "wv_extend_type")
 
-            newrow(layout, 'Twist:', swv, "wv_rotate")    
+            newrow(layout, 'Twist:', swv, "wv_rotate")   
+            newrow(layout, 'Jitter level:', swv, "wv_jitter")
+
+            if swv.wv_jitter:
+                newrow(layout, 'Jitter type:', swv, "wv_jitter_type")
+
             newrow(layout, 'Segments:', swv, "wv_seg")
             newrow(layout, 'Diameter:', swv, "wv_dia")
 
@@ -150,6 +162,11 @@ class WIREVIS_PT_object(Panel):
                 newrow(layout, 'Extend type:',  owv,  "wv_extend_type")
             
             newrow(layout, 'Twist:',  owv,  "wv_rotate")
+            newrow(layout, 'Jitter level:', owv, "wv_jitter")
+
+            if owv.wv_jitter:
+                newrow(layout, 'Jitter type:', owv, "wv_jitter_type")
+
             newrow(layout, 'Segments:',  owv,  "wv_seg")
             newrow(layout, 'Diameter:',  owv,  "wv_dia")
 
@@ -165,7 +182,6 @@ class VIEW3D_OT_WireVis(Operator):
         wm = context.window_manager
         scene = context.scene
         wvp = scene.wv_params
-        # self.draw_handle_wv = SpaceView3D.draw_handler_add(self.draw_wv, (self, context), "WINDOW", "POST_VIEW")
         self._timer = wm.event_timer_add(0.05, window=context.window)
         wm.modal_handler_add(self)
         wvp.wv_display = 1
@@ -195,13 +211,19 @@ class VIEW3D_OT_WireVis(Operator):
                 wbm = bmesh.new()
                 bmesh.ops.create_cone(wbm, cap_ends=1, cap_tris=1, depth=1, segments=swv.wv_seg, radius1=swv.wv_dia * 0.001, radius2=swv.wv_dia * 0.001)
                 ows = swv
+                wbm.verts.ensure_lookup_table()
+                low_z = wbm.verts[0].co[2]
+                hi_z = wbm.verts[1].co[2]
 
             for oi, ob in enumerate(wv_obs):
                 if not swv.wv_override:
                     ows = ob.wirevis_settings
                     wbm = bmesh.new()
                     bmesh.ops.create_cone(wbm, cap_ends=1, cap_tris=1, depth=1, segments=ows.wv_seg, radius1=ows.wv_dia * 0.001, radius2=ows.wv_dia * 0.001)
-
+                
+                wbm.verts.ensure_lookup_table()
+                low_z = wbm.verts[0].co[2]
+                hi_z = wbm.verts[1].co[2]
                 obbm = bmesh.new()
                 obbm.from_object(ob, dp)
                 obbm.transform(ob.matrix_world)
@@ -217,8 +239,10 @@ class VIEW3D_OT_WireVis(Operator):
                     mat_rot = mathutils.Vector((0, 0, 1)).rotation_difference(ecoords[ei][1] - ec).to_matrix().to_4x4()
                     nbm = wbm.copy()
                     nbm.verts.ensure_lookup_table()
-                    low_z = nbm.verts[0].co[2]
-                    hi_z = nbm.verts[1].co[2]
+
+                    if ows.wv_jitter and ows.wv_jitter_type == '0':
+                        rand1 = random.uniform(-ows.wv_jitter * 0.01, ows.wv_jitter * 0.01)
+                        rand2 = random.uniform(-ows.wv_jitter * 0.01, ows.wv_jitter * 0.01)
 
                     if ows.wv_rotate > 0:
                         bmesh.ops.rotate(nbm, cent=nbm.verts[1].co, matrix=mathutils.Matrix.Rotation(radians(ows.wv_rotate), 4, 'Z'), verts=[v for v in nbm.verts if v.co[2] == hi_z])
@@ -226,8 +250,23 @@ class VIEW3D_OT_WireVis(Operator):
                     for v in nbm.verts:
                         if v.co[2] == low_z:
                             v.co[2] = -e_lens[ei] * 0.5
+
+                            if ows.wv_jitter and ows.wv_jitter_type == '0':
+                                v.co[0] += rand1
+                                v.co[1] += rand1
+                            elif ows.wv_jitter:
+                                v.co[0] += random.uniform(-ows.wv_jitter * 0.01, ows.wv_jitter * 0.01)
+                                v.co[1] += random.uniform(-ows.wv_jitter * 0.01, ows.wv_jitter * 0.01)
+
                         elif v.co[2] == hi_z:
                             v.co[2] = e_lens[ei] * 0.5
+
+                            if ows.wv_jitter and ows.wv_jitter_type == '0':
+                                v.co[0] += rand2
+                                v.co[1] += rand2
+                            elif ows.wv_jitter:
+                                v.co[0] += random.uniform(-ows.wv_jitter * 0.01, ows.wv_jitter * 0.01)
+                                v.co[1] += random.uniform(-ows.wv_jitter * 0.01, ows.wv_jitter * 0.01)
 
                     if ows.wv_extend > 0:
                         if ows.wv_extend_type == '0':
@@ -243,30 +282,45 @@ class VIEW3D_OT_WireVis(Operator):
                     mis_out += [oi for f in nbm.faces]
                     nbm.free()
                 
-                try:
-                    wire_material = bpy.data.materials[f'wire_material-{oi}']
-                except Exception:
-                    wire_material = bpy.data.materials.new(name=f'wire_material-{oi}')
-
-                wire_material.diffuse_color = ows.wv_colour
-                obbm.free()
-
                 if not swv.wv_override:
                     wbm.free()
 
+                    try:
+                        wire_material = bpy.data.materials[f'wire_material-{oi}']
+                    except Exception:
+                        wire_material = bpy.data.materials.new(name=f'wire_material-{oi}')
+                
+                    wire_material.diffuse_color = ows.wv_colour
+                
+                obbm.free()
+
             if swv.wv_override:
                 wbm.free()
+                
+                try:
+                    wire_material = bpy.data.materials[f'wire_material-0']
+                except Exception:
+                    wire_material = bpy.data.materials.new(name=f'wire_material-0')
 
             try:
                 wire_object = bpy.data.objects['wire_object']
                 wire_object.data.clear_geometry()
                 wire_object.data.from_pydata(verts_out, [], faces_out)
-                wire_object.data.polygons.foreach_set('material_index', mis_out)
+                
+                if swv.wv_override:
+                    wire_object.data.polygons.foreach_set('material_index', [0 for mo in mis_out])
+                else:
+                    wire_object.data.polygons.foreach_set('material_index', mis_out)
 
             except Exception:
                 wire_mesh = bpy.data.meshes.new('wire_mesh')
                 wire_mesh.from_pydata(verts_out, [], faces_out)
-                wire_mesh.polygons.foreach_set('material_index', mis_out)
+                
+                if swv.wv_override:
+                    wire_mesh.polygons.foreach_set('material_index', [0 for mo in mis_out])
+                else:
+                    wire_mesh.polygons.foreach_set('material_index', mis_out)
+
                 wire_object = bpy.data.objects.new("wire_object", wire_mesh)
                 bpy.context.collection.objects.link(wire_object)
                 wire_object.display.show_shadows = False
@@ -290,105 +344,6 @@ class VIEW3D_OT_WireVis(Operator):
                 wire_object.material_slots[oi].material.diffuse_color = colour
 
         return {'PASS_THROUGH'}
-
-
-# class VIEW3D_OT_WireVis_OLD(Operator):
-#     bl_idname = "view3d.wirevis_old"
-#     bl_label = "Wire Vis"
-#     bl_description = "Display Wireframe"
-#     bl_register = True
-#     bl_undo = False
-
-#     def invoke(self, context, event):
-#         scene = context.scene
-#         wvp = scene.wv_params
-#         # self.draw_handle_wv = SpaceView3D.draw_handler_add(self.draw_wv, (self, context), "WINDOW", "POST_VIEW")
-#         context.window_manager.modal_handler_add(self)
-#         wvp.wv_display = 1
-#         wvp.update = 1
-#         return {'RUNNING_MODAL'}
-
-#     def modal(self, context, event):
-#         scene = context.scene
-#         wvp = scene.wv_params
-#         dp = context.evaluated_depsgraph_get()
-#         wv_obs = [ob for ob in scene.objects if ob.wirevis_settings.wv_bool]
-
-#         if not wvp.wv_display:
-#             return {'FINISHED'}
-
-#         if wvp.update:
-#             bm = bmesh.new()
-
-#             if not wv_obs:
-#                 self.report({'ERROR'}, 'No objects with the wire_vis display property turned on')
-#                 return {'CANCELLED'}
-
-#             for oi, ob in enumerate(wv_obs):
-#                 ows = ob.wirevis_settings
-#                 obbm = bmesh.new()
-#                 obbm.from_object(ob, dp)
-#                 obbm.transform(ob.matrix_world)
-#                 ecoords = [[v.co for v in e.verts] for e in obbm.edges if (e.calc_face_angle(0.1) * 180/pi > ows.wv_angle or
-#                            (ows.wv_material and len(e.link_faces) == 2 and len(set(f.material_index for f in e.link_faces)) == 2) or
-#                            (ows.wv_le and len(e.link_faces) == 1)) and e.calc_length()*1000 > ows.wv_len]
-#                 ecentres = [(e[0] + e[1]) * 0.5 for e in ecoords]
-
-#                 for ei, ec in enumerate(ecentres):
-#                     length = (ecoords[ei][0] - ecoords[ei][1]).length
-#                     mat_trans = mathutils.Matrix.Translation(ec)
-#                     mat_rot = mathutils.Vector((0, 0, 1)).rotation_difference(ecoords[ei][1] - ec).to_matrix().to_4x4()
-#                     wire_verts = bmesh.ops.create_cone(bm, cap_ends=1, cap_tris=1, segments=ows.wv_seg, radius1=ows.wv_dia * 0.001,
-#                                                        radius2=ows.wv_dia * 0.001, depth=length, matrix=mat_trans@mat_rot)['verts']
-
-#                     for v in wire_verts:
-#                         for f in v.link_faces:
-#                             f.material_index = oi
-
-#                     if ows.wv_extend > 0:
-#                         wire_verts[0].co += (wire_verts[0].co - ec).normalized() * ows.wv_extend
-#                         wire_verts[1].co += (wire_verts[1].co - ec).normalized() * ows.wv_extend
-
-#                 obbm.free()
-
-#                 try:
-#                     wire_material = bpy.data.materials[f'wire_material-{oi}']
-#                 except Exception:
-#                     wire_material = bpy.data.materials.new(name=f'wire_material-{oi}')
-
-#                 wire_material.diffuse_color = ows.wv_colour
-
-#             try:
-#                 wire_object = bpy.data.objects['wire_object']
-
-#             except Exception:
-#                 wire_mesh = bpy.data.meshes.new('wire_mesh')
-#                 wire_object = bpy.data.objects.new("wire_object", wire_mesh)
-#                 bpy.context.collection.objects.link(wire_object)
-#                 wire_object.display.show_shadows = False
-
-#             bm.transform(wire_object.matrix_world.inverted())
-#             bm.to_mesh(wire_object.data)
-
-#             for i in range(oi + 1):
-#                 while i >= len(wire_object.material_slots):
-#                     wire_object.data.materials.append(bpy.data.materials[f'wire_material-{i}'])
-
-#             bm.free()
-#             wvp.update = 0
-
-#             for area in context.window.screen.areas:
-#                 if area.type == 'VIEW_3D':
-#                     area.tag_redraw()
-
-#         elif wvp.cupdate:
-#             wire_object = bpy.data.objects['wire_object']
-#             wvp.cupdate = 0
-
-#             for oi, ob in enumerate(wv_obs):
-#                 wire_object.material_slots[oi].material.diffuse_color = ob.wirevis_settings.wv_colour
-
-#         return {'PASS_THROUGH'}
 
 
 class VIEW3D_OT_WireCancel(Operator):
